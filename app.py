7eb37b545cc4d10587d57019116b2454f4ee221d
nwactor <nickwactor@gmail.com>
2018-09-24 15:59:32 -0700

    sending data to server and recieving response

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import (Flask,render_template)
+import sys
+from flask import (Flask,render_template,jsonify,request)
+
 
 app = Flask(__name__, template_folder = "./views")
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/postData', methods=['POST'])
+def process_data():
+	print(request.json['selectedFeature'], file=sys.stdout)
+	return jsonify({})
 
 if __name__ == '__main__':
     app.run(debug = True)
--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,12 @@
+var userData = {};
+
 function parseFeatures(files){
     //selects the first file
     //possible to send many files, but for now we will only use one
     var data = Papa.parse(files[0],{
         complete: function(results){
-
-            console.log('finished',results.data[0]);
+            userData.file = results;
+            // console.log('finished',results.data[0]);
             // return results.data[0];
             document.getElementById('featureSelect').style.visibility = 'visible';
             results.data[0].forEach(function(feature){
@@ -15,5 +17,25 @@ function parseFeatures(files){
             });
         }
     });
-
 };
+
+function onFeatureSelect() {
+
+}
+
+function postUserData() {
+    var selector = document.getElementById('featureSelect');
+    userData.selectedFeature = selector.options[selector.selectedIndex].value;
+    
+    var request = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+    }
+    fetch("/postData", request).then(function(response) {
+        console.log(response);
+    });
+}
